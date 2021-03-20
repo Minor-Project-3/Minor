c052da32cf3cb74eaf69fff8f36a1438dbf944ac
PRANAV1199 <64365123+PRANAV1199@users.noreply.github.com>
2021-03-20 17:08:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,90 +1,85 @@
-from flask import Flask, render_template, request,session,flash
+from flask import Flask,render_template,request,session,flash,redirect,url_for,send_from_directory,flash
+from email.mime.text import MIMEText
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-import random
-import json
-import os
+from sqlalchemy.sql import func
+import random, copy
+import smtplib
+import requests
 import string
-# from werkzeug import *
+import random
 from werkzeug.utils import secure_filename
+import os
+from database import db,User,Post,follows,liketab
+from flask_mail import Mail
 from datetime import datetime
+from passlib.hash import sha256_crypt
+import tensorflow as tf
+import joblib
+import numpy as np
+from keras.preprocessing import image
 
-with open('config.json', 'r') as c:
-    parameter = json.load(c)["parameter"]
 
-app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/fakenews'
-app.config['UPLOAD_FOLDER']=parameter['folder_location']
-app.config['SQLALCHEMY_DATABASE_URI'] = parameter['database_url']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'the random string'
-db = SQLAlchemy(app)
 
+app=Flask(__name__)
+loaded_model = joblib.load('./pipeline.sav')
+
+model = tf.keras.models.load_model('model (1).h5') 
+app.config['UPLOAD_FOLDER']=r'static\uploads'
+app.secret_key='United'
+#app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:saksham@localhost/minor'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://ykznxsdzcebfil:d1f44feba03fada8da02d88a302da48ac1544efbbf301f9a47361ac1fde07ea9@ec2-54-159-175-113.compute-1.amazonaws.com:5432/ddqgp9nsn8aj32?sslmode=require'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = '465',
     MAIL_USE_SSL = True,
-    MAIL_USERNAME = parameter['admin_mail'],
-    MAIL_PASSWORD=  parameter['admin_pass']
+    MAIL_USERNAME = "psp51790@gmail.com",
+    MAIL_PASSWORD=  "ps*123456"
 )
 mail = Mail(app)
+
 i=0
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-follows=db.Table('follows',db.Column('uid_who',db.Integer,db.ForeignKey('user.uid'),primary_key=True),db.Column('uid_whom',db.Integer,db.ForeignKey('user.uid'),primary_key=True))
-liketab=db.Table('liketab',db.Column('uid_who',db.Integer,db.ForeignKey('user.uid'),primary_key=True),db.Column('pid',db.Integer,db.ForeignKey('post.pid'),primary_key=True),db.Column('like_date',db.DateTime))
+@app.route("/",methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
 
-class User(db.Model):
-    __tablename__="user"
-    uid=db.Column(db.Integer,primary_key=True, unique=True,autoincrement=True)
-    uname=db.Column(db.String(120), nullable=False)
-    passw=db.Column(db.String(120), nullable=False)
-    name=db.Column(db.String(120), nullable=False)
-    email=db.Column(db.String(120), nullable=False)
-    ContactNo=db.Column(db.String(120), nullable=False)
-    follower_count=db.Column(db.Integer)
-    following_count=db.Column(db.Integer)
-    descrp=db.Column(db.String(220))
-    terror_count=db.Column(db.Integer)
-    posts=db.relationship('Post',backref='user')
-    #follo=db.relationship('User',secondary=follows)
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if(request.method=='POST'):
+        Custname = request.form.get('user')
+        Custpass = request.form.get('pass')
+        getinfo = db.session.query(User).filter_by(uname=Custname).first()
+        if Custname=="admin" and Custpass=="admin":
+            session['user'] = "admin"
+            return render_template('admindashboard.html',name=Custname)
+        elif sha256_crypt.verify(Custpass, getinfo.passw):
+            session['user'] = Custname
+            session['uid']=getinfo.uid
+            b=session['uid']
+            a = db.session.execute("SELECT * FROM follows WHERE uid_who=:param ",{"param": b})
+            names = [row[1] for row in a]
+            c,d=[],[]
+            for i in names :
+                getinfo = db.session.query(User).filter_by(uid=i).order_by(func.random()).all()
+                c.append(getinfo[0])
+            for j in c:
+                get = db.session.query(Post).filter_by(uid=j.uid,active=1).order_by(Post.pdate.desc()).all()
+                d.append(get)
+                        
+            return render_template("homepage.html",unam=session['user'],c=zip(c,d))
+        else:
+            return render_template('index.html')
 
-    def __init__(self,passw,uName,name,cont,ema,descrp,follower_count=0,following_count=0,terror_count=0):
-        self.passw=passw
-        self.uname=uName
-        self.name=name
-        self.ContactNo=cont
-        self.email=ema
-        self.follower_count=follower_count
-        self.following_count=following_count
-        self.descrp=descrp
-        self.terror_count=terror_count
+@app.route("/logout")  
+def logout():
+    print(session['user'])
+    session.pop('user', None)
+    return redirect("/")
 
-class Post(db.Model):
-    __tablename__="post"
-    pid=db.Column(db.Integer,primary_key=True, unique=True,autoincrement=True)
-    ptitle=db.Column(db.String(120),nullable=False)
-    pdate=db.Column(db.DateTime)
-    pdesc=db.Column(db.String(500))
-    pimgpath=db.Column(db.String(120))
-    likes=db.Column(db.Integer)
-    active=db.Column(db.Integer)
-    uid=db.Column(db.Integer,db.ForeignKey('user.uid'),nullable=False)
-#lik=db.relationship('user',secondary=liketab)
-
-def __init__(self, ptitle,pdate,pdesc,uid,pimgpath,likes=0,active=1):
-    self.ptitle=ptitle
-    self.pdate=pdate
-    self.pdesc=pdesc
-    self.pimgpath=pimgpath
-    self.likes=likes
-    self.active=active
-    self.uid=uid
-
-@app.route("/")
-def func():
-    return render_template('index.html')
-
-# -----------------------------------------------------------------------
 
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
@@ -95,87 +90,211 @@ def signup():
         rpassw = request.form.get('rpassw')
         email= request.form.get('email')
         phno = request.form.get('phno')
-        if passw==rpassw:
-            user1=User(passw=passw,uName=uname,name=name,cont=phno,ema=email,descrp="Hi There I am using Fake Book")
+        info=db.session.query(User).filter_by(uname=uname).count()
+        if passw==rpassw and info!=1:
+            passs=sha256_crypt.encrypt(passw)
+            user1=User(passw=passs,uName=uname,name=name,cont=phno,ema=email,descrp="Hi There I am using Fake Book")
             db.session.add(user1)
             db.session.commit()
-            return render_template('index.html',tick=1)
-    return"HEllo"
-
-@app.route("/login", methods = ['GET', 'POST'])
-def login():
-    if not session.get('logged_in'):
-        if(request.method=='POST'):
-            Custname = request.form.get('user')
-            Custpass = request.form.get('pass')
-            getinfo = db.session.query(User).filter_by(uname=Custname,passw=Custpass).count()
-            if getinfo==1:
-                session['logged_in'] = True
-                return render_template('dashboard.html',my_string=Custname)
-            else:
-                return render_template('index.html',val=1)
-    else:
-        return render_template("home.html")
-    
-
-@app.route("/logout")  
-def logout():
-    session['logged_in'] = False
-    return render_template("index.html")
+            return render_template("index.html",mess="Congratulation,Account has been created")
+        else:
+            return "Username Already Exist"
 
 @app.route("/forget",methods = ['GET', 'POST'])
 def forget():
     return render_template("forget.html")
-
+    
 @app.route("/reset",methods = ['GET', 'POST'])  
 def reset():
     uname=request.form.get('uname')
     email = request.form.get('email')
-    getinfo = db.session.query(User).filter_by(uname=uname,email=email)
-    # smail="psp51790@gmail.com"
-    smail=parameter['admin_mail']
+    getinfo = User.query.filter_by(uname=uname,email=email)
+    smail="psp51790@gmail.com"
     if getinfo.count()==1:
         pas=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-        getinfo.update(dict(passw=pas))
+        type(getinfo)
         message="Hello There %s .<br>Your Password has been generated .<br>Your password is <strong>%s</strong>.<br>Please login with this password to change password"%(uname,pas)
         #msg=MIMEText(message,'html')
+        getinfo.update(dict(passw=pas))
         db.session.commit()
         mail.send_message(subject="Password Generated",html=message,sender=smail,recipients = [email])
         return "Hello"
     else:
-        return "Hi"
-
-# -----------------------------------------------------------------------------------------------------------
+        return "Hi"  
 
 @app.route("/upload",methods=['POST','GET'])
 def upload():
     if (request.method =="POST"):
         f=request.files['file']
         ptitle=request.form.get('ptitle')
-        global i
-        i=i+1
+        pdesc=request.form.get('desc')
+        i=0
         f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
-        # path=str(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename+str(i)))
         path=str(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
-        # print(path) 
-        post=Post(ptitle=ptitle,pdate=datetime.now(),pdesc="Hello",uid=1,pimgpath=path,likes=0,active=1)  
+        print(path)
+        result = loaded_model.predict([pdesc])
+        print(result)
+        test_image = image.load_img(path, target_size = (64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result1 = model.predict(test_image)
+        if result1[0][0] == 1:
+            prediction = 'terror'
+            i=0   
+        else:
+            prediction = 'person'
+            i=1
+        print(prediction) 
+        post=Post(ptitle=ptitle,pdate=datetime.now(),pdesc=pdesc,uid=session['uid'],pimgpath=path,likes=0,active=i)  
         db.session.add(post)
         db.session.commit()
-        return "uploaded"
+        return render_template("homepage.html",unam=session['user'],mesg="Post Uploaded Successfully")
     else:
         return "sorry"
 
 @app.route("/uploadPost",methods=['POST','GET'])
 def uploadPost():
-    return render_template("upload.html")
+    return render_template("upload.html")    
 
-# -----------------------------------------------------------------------------------------------------------
+@app.route("/displayPost/<posttype>",methods=['POST','GET'])
+def displayPost(posttype):
+    if posttype=="fair":
+        info=db.session.query(Post).filter_by(active=1).all()
+        return render_template("test.html",post=info)
+    if posttype=="blocked":
+        info=db.session.query(Post).filter_by(active=0).all()
+        return render_template("test.html",post=info)
+    if posttype=="critical":
+        info=db.session.query(User).filter_by(terror_count=2).all()
+        return render_template("userlist.html",info=info)
+    if posttype=="blockedusr":
+        info=db.session.query(User).filter_by(terror_count=3).all()
+        return render_template("userlist.html",info=info)
 
-@app.route("/retrivePost",methods=['POST','GET'])
-def retrivePost():
-    post=Post.query.first()
-    return render_template("postRetrive.html",post=post)
+@app.route('/display/<filename>')
+def display_image(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename=filename)
 
+@app.route('/userlist')
+def userlist():
+    info=db.session.query(User)
+    return render_template('userlist.html',info=info)
 
-if __name__ == "__main__":
+@app.route("/adminDashboard",methods=['POST','GET'])
+def adminDashBoard():
+    return render_template("upload.html") 
+
+@app.route("/deletePost/<string:id>",methods=['POST'])
+def deletePost(id):
+    db.session.query(Post).filter_by(pid=id).delete()
+    db.session.commit()
+    return render_template('admindashboard.html')
+
+@app.route("/followUser",methods=['POST'])
+def followUser():
+    getinfo = db.session.query(User).all()
+    return render_template("Following.html",users=getinfo)  
+
+ 
+
+@app.route("/follow/<string:id>",methods=['POST'])
+def follow(id):
+    user=session['uid']
+    statement = follows.insert().values(uid_who=user, uid_whom=id)
+    db.session.execute(statement)
+    db.session.commit()
+    getinfo=db.session.query(User).filter_by(uid=user)
+    getinfo1=db.session.query(User).filter_by(uid=id)
+    i=getinfo.first().following_count
+    i+=1
+    getinfo.update({User.following_count:i})
+    k=getinfo1.first().follower_count
+    k+=1
+    getinfo1.update({User.follower_count:k})
+    db.session.commit()
+    a = db.session.execute("SELECT * FROM follows WHERE uid_who=:param ",{"param": user})
+    names = [row[1] for row in a]
+    c,d=[],[]
+    for i in names :
+        getinfo = db.session.query(User).filter_by(uid=i).order_by(func.random()).all()
+        c.append(getinfo[0])
+    for j in c:
+        get = db.session.query(Post).filter_by(uid=j.uid,active=1).order_by(Post.pdate.desc()).all()
+        d.append(get)
+                        
+    return render_template("homepage.html",unam=session['user'],c=zip(c,d))
+    
+
+@app.route("/followingList",methods=['GET'])
+def followingList():
+    b=session['uid']
+    # info=follows.select().where(uid_who==b)
+    a = db.session.execute("SELECT * FROM follows WHERE uid_who=:param",{"param": b})
+    names = [row[1] for row in a]
+    c=[]
+    for i in names :
+        getinfo = db.session.query(User).filter_by(uid=i).all()
+        c.append(getinfo[0])
+    print(c[0].uid)
+    # print(getinfo[0].uid)
+    return render_template("unfollow.html",users=c)
+@app.route("/followerList",methods=['GET'])
+def followerList():
+    b=session['uid']
+    # info=follows.select().where(uid_who==b)
+    a = db.session.execute("SELECT * FROM follows WHERE uid_whom=:param",{"param": b})
+    names = [row[1] for row in a]
+    c=[]
+    for i in names :
+        getinfo = db.session.query(User).filter_by(uid=i).all()
+        c.append(getinfo[0])
+    print(c[0].uid)
+    # print(getinfo[0].uid)
+    return render_template("Following.html",users=c)
+
+@app.route("/unfollow/<string:id>",methods=['POST'])
+def unfollow(id):
+    user=session['uid']
+    # statement = follows.delete(uid_who=a, uid_whom=id)
+    a = db.session.execute("DELETE FROM follows WHERE uid_who=:param and uid_whom=:para ",{"param": user,"para":id})
+    # db.session.execute(statement)
+    db.session.commit()
+    getinfo=db.session.query(User).filter_by(uid=user)
+    i=getinfo.first().following_count
+    i-=1
+    getinfo.update({User.following_count:i})
+
+    getinfo1=db.session.query(User).filter_by(uid=id)
+    k=getinfo1.first().follower_count
+    k-=1
+    getinfo1.update({User.follower_count:k})
+
+    db.session.commit()
+    a = db.session.execute("SELECT * FROM follows WHERE uid_who=:param ",{"param": user})
+    names = [row[1] for row in a]
+    c,d=[],[]
+    for i in names :
+        getinfo = db.session.query(User).filter_by(uid=i).order_by(func.random()).all()
+        c.append(getinfo[0])
+    for j in c:
+        get = db.session.query(Post).filter_by(uid=j.uid,active=1).order_by(Post.pdate.desc()).all()
+        d.append(get)
+                    
+    return render_template("homepage.html",unam=session['user'],c=zip(c,d))
+
+@app.route("/Myprofile",methods=['GET'])
+def Myprofile():
+    getinfo = db.session.query(User).filter_by(uid=session['uid']).first()
+    getpost = db.session.query(Post).filter_by(uid=session['uid'],active=1).order_by(Post.pdate.desc()).all()
+    return render_template("Myprofile.html",users=getinfo,post=getpost)
+
+@app.route("/like",methods=['POST','GET'])
+def like():
+    if (request.method =="POST"):
+        f=request.form['pid']
+        print(f)
+        return "H"
+        
+if __name__=="__main__":
     app.run(debug=True)
+
